d1e339fa2514e446ea40b26af724dec7efff3571
Lev Anufriev <levanufriev02@gmail.com>
2022-12-31 12:19:48 +0300

    Add changes.

--- src/components/Farm.js
+++ src/components/Farm.js
@@ -1,22 +1,52 @@
-import React from 'react'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import './Farm.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 export default function Farm() {
+    const [data, setData] = useState({
+        name: ""
+    })
+
+    const instance = axios.create({
+        baseURL: 'https://localhost:44333/api/',
+        headers: {'Authorization': 'Bearer '+ localStorage.getItem('token')}
+      });
+
+    const farm = axios.get('/farms').then(result => {
+        console.log(result);
+    });
+
+    function submit(e) {
+        e.preventDefault();
+        instance.post('farms',{
+            name: data.name
+        }).then(result => {
+            console.log(result)
+        }).catch(function (error) {
+            if (error.response) {
+              console.log(error.response.data);
+            } 
+          });
+    }
+
+    function handle(e) {
+        const newData = {...data}
+        newData[e.target.id] = e.target.value
+        setData(newData)
+    }    
+
+    if (farm.data == null)
+    {
+        return(
+            <form onSubmit={(e) => create(e)}>
+                <input type="text" placeholder="Enter name" onChange={(e) => handle(e)} id="name" value={data.name}></input>
+                <button>Create</button>
+            </form>
+        )
+    }
+
     return (
-        <Row className="align-items-center">
-            <Col>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Col>
-            <Col>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Col>
-        </Row>
+        <div>
+            <div>{data.name}</div>
+        </div>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,47 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React, {useState} from 'react'
+import axios from 'axios';
 
 export default function Login() {
-    return (
-        <div align="center">
-            <Form>
-            <Form.Group className="mb-3 px-5" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+    const url = "https://localhost:44333/api/authentication/login"
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    })
 
-            <Form.Group className="mb-3 px-5" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-            </Form>
+    const instance = axios.create({
+        baseURL: 'https://localhost:44333/api/'
+      });
+
+    function submit(e) {
+        e.preventDefault();
+        instance.post('authentication/login',{
+            email: data.email,
+            password: data.password
+        }).then(result => {
+            console.log(result)
+            const token = result.data.token
+            localStorage.setItem('token', token)
+            console.log(localStorage.getItem('token'))
+        }).catch(function (error) {
+            if (error.response) {
+              console.log(error.response.data);
+            } 
+          });
+    }
+
+    function handle(e) {
+        const newData = {...data}
+        newData[e.target.id] = e.target.value
+        setData(newData)
+        console.log(newData)
+    }
+    return (
+        <div>
+            <form onSubmit={(e) => submit(e)}>
+                <input type="email" placeholder="Enter email" onChange={(e) => handle(e)} id="email" value={data.email}></input>
+                <input type="password" placeholder="Enter password" onChange={(e) => handle(e)} id="password" value={data.password}></input>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
