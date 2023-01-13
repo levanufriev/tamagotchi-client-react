d2aff40cc96dca66764a032360ce5e05ac4eb8e6
Lev Anufriev <levanufriev02@gmail.com>
2023-01-13 21:29:04 +0300

    Change login page.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Navigation} from './components/Navigation'
 import Login from './components/Login'
 import Register from './components/Register'
 import Farm from './components/Farm'
+import Pet from './components/Pet.js'
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
@@ -13,11 +14,8 @@ function App() {
   return (
     <BrowserRouter>
       <div className="container">
-
-        
-      <Navigation/>
         <Routes>
-          <Route exact path='/' element={<Home/>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/farm' element={<Farm/>}/>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react'
-import axios from 'axios';
+import axios from 'axios'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function Login() {
-    const url = "https://localhost:44333/api/authentication/login"
     const [data, setData] = useState({
         email: "",
         password: ""
@@ -21,11 +22,20 @@ export default function Login() {
             console.log(result)
             const token = result.data.token
             localStorage.setItem('token', token)
-            console.log(localStorage.getItem('token'))
         }).catch(function (error) {
             if (error.response) {
-              console.log(error.response.data);
-            } 
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+                if (error.response.status == 401) {
+                    alert("Wrong email or password.");
+                }
+              } else if (error.request) {
+                console.log(error.request);
+              } else {
+                console.log('Error', error.message);
+              }
+              console.log(error.config);
           });
     }
 
@@ -35,13 +45,28 @@ export default function Login() {
         setData(newData)
         console.log(newData)
     }
+
     return (
-        <div>
-            <form onSubmit={(e) => submit(e)}>
-                <input type="email" placeholder="Enter email" onChange={(e) => handle(e)} id="email" value={data.email}></input>
-                <input type="password" placeholder="Enter password" onChange={(e) => handle(e)} id="password" value={data.password}></input>
-                <button>Submit</button>
-            </form>
+        <div align="center">
+            <Form className="mb-3" onSubmit={(e) => submit(e)}>
+                <Form.Group className="mb-3 px-5">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" onChange={(e) => handle(e)} id="email" value={data.email}/>
+                </Form.Group>
+
+                <Form.Group className="mb-3 px-5">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={(e) => handle(e)} id="password" value={data.password}/>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+
+            <Button href="/register" variant="primary" type="submit">
+                Register
+            </Button>
         </div>
     )
 }
