3f12f2588773877235b182578634f46092be6753
Lev Anufriev <levanufriev02@gmail.com>
2023-01-16 17:28:54 +0300

    Add farm details page.

--- src/components/FarmDetails.js
+++ src/components/FarmDetails.js
@@ -1,16 +1,25 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import Image from "react-bootstrap/Image";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function FarmsOverview() {
-    const [data, setData] = useState({
-        name: "",
-        petsAmount: 0
-    })
+    const [data, setData] = useState([])
+    const [farmId, setFarmId] = useState("")
+    const [farmGetCompleted, setFarmGetCompleted] = useState(false)
 
     useEffect(() => {
         getFarm();
-        getPets();
     }, [])
+
+    useEffect(() => {
+        if (farmGetCompleted) {
+            getPets();
+        }
+    }, [farmGetCompleted])
 
     const instance = axios.create({
         baseURL: 'https://localhost:44333/api',
@@ -19,8 +28,9 @@ export default function FarmsOverview() {
 
     function getFarm() {
         instance.get('/farms').then(result => {
-            console.log(result);
-            localStorage.setItem('farmId', result.data.id)
+            setFarmId(result.data.id)
+            console.log(result.data.id)
+            setFarmGetCompleted(true)
         }).catch(error => {
             if (error.response) {
                 console.log(error.response.data);
@@ -36,10 +46,25 @@ export default function FarmsOverview() {
     }    
 
     function getPets() {
-        const farmId = localStorage.getItem('farmId');
+        instance.get('/farms').then(result => {
+            console.log(result);
+            setFarmId(result.data.id)
+        }).catch(error => {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                console.log(error.request);
+              } else {
+                console.log('Error', error.message);
+              }
+              console.log(error.config);
+        });
+        console.log(farmId)
         instance.get('/farms/'+farmId+'/pets').then(result => {
             console.log(result);
-            setData({name: result.data.name, petsAmount: result.data.petsAmount})
+            setData(result.data)
         }).catch(error => {
             if (error.response) {
                 console.log(error.response.data);
@@ -73,11 +98,30 @@ export default function FarmsOverview() {
         setData(newData)
     }
 
+    const arr = data.map((data, index) => {
+        return (
+            <Col md={4}>            
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Image src={data.eyesImageSrc} width="100" height="100"/><br/>
+                    <Image src={data.noseImageSrc} width="100" height="100"/><br/>
+                    <Image src={data.mouthImageSrc} width="100" height="100"/><br/>
+                    <Image src={data.bodyImageSrc} width="100" height="100"/><br/>
+                    <h1>{data.age}</h1>
+                </Card.Body>
+            </Card>
+            </Col>
+        )
+    })
+
     
     return(
         <div>
-            <h1>{data.name}</h1>
-            <h1>{data.petsAmount}</h1>
+            <Container>
+                <Row>
+                    {arr}
+                </Row>
+            </Container>
         </div>
     )
 }
